43027b5c4f4c432e14ed91d9c68e2eca8a3529f6
tomo <stomo834@gmail.com>
2020-10-25 01:37:41 +0900

    fix excel import

--- app/Imports/AdminsImport.php
+++ app/Imports/AdminsImport.php
@@ -39,6 +39,25 @@ class AdminsImport implements ToCollection, WithStartRow
         {
 
             $row = $row->toArray();
+            
+
+            $data = [
+                "request" => $row[0],
+                "name" => $row[1],
+                "email" => $row[10],
+                "incentive" => $row[24],
+                "mailing_date" => $row[31],
+            ];
+            $conditions = [
+                "request" => "date",
+                "name" => "required",
+                "email" => "required|email|unique:admins",
+                "incentive" => "required|integer",
+                "mailing_date" => "date",
+            ];
+            
+            Validator::make($data, $conditions)->validate();
+            
             $p = str_random(12);
 
             if ($row[15] != ""){
@@ -88,23 +107,6 @@ class AdminsImport implements ToCollection, WithStartRow
             }else{
                 $mailing_date = $row[31];
             }
-            
-            $data = [
-                "request" => $row[0],
-                "name" => $row[1],
-                "email" => $row[10],
-                "incentive" => $row[24],
-            ];
-            $conditions = [
-                "request" => "date",
-                "name" => "required",
-                "email" => "required|email|unique:admins",
-                "incentive" => "required|integer"
-            ];
-            
-            Validator::make($data, $conditions)->validate();
-            
-            
 
             $admin = Admin::create([
                 'name' => $row[1],
