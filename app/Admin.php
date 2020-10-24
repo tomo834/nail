f07c2c10f1008799abcc1a89f5b42c1a18faeb97
tomo <stomo834@gmail.com>
2020-10-24 20:37:20 +0900

    fix migration-file

--- app/Admin.php
+++ app/Admin.php
@@ -5,6 +5,7 @@ namespace App;
 use App\Notifications\AdminResetPassword;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Log;
 
 class Admin extends Authenticatable
 {
@@ -16,9 +17,8 @@ class Admin extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','type', 'name_jp',"company_name","representative","shop_name","shop_pic", "birthday","address","phone","fax","cellphone","account_holder","bank_name","branch_name","bank_code","branch_code","account_type","account_number","incentive","need_file","historical_matters","seal_certificate","passbook","residents_card","other","mailing_date","account_id"
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -37,5 +37,15 @@ class Admin extends Authenticatable
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new AdminResetPassword($token));
+    }
+
+    public function incentive_info(){
+        return $this->hasMany("App\Incentive");
+    }
+    public function product_incentive_info(){
+        return $this->hasMany("App\ProductIncentive");
+    }
+    public function user_info(){
+        return $this->hasMany("App\User");
     }
 }
