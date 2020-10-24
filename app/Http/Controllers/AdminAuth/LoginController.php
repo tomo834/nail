8b957aad32c1bf02674ce034c238469f588bc711
tomo <stomo834@gmail.com>
2020-10-24 22:28:53 +0900

    fix admin logout

--- app/Http/Controllers/AdminAuth/LoginController.php
+++ app/Http/Controllers/AdminAuth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    public $redirectTo = '/admin/home';
+    public $redirectTo = '/admin/login';
 
     /**
      * Create a new controller instance.
