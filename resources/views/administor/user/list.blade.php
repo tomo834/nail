690b2b83c7995c8647907c5d1a1b237a0d23ba86
tomo <stomo834@gmail.com>
2020-10-26 07:52:56 +0900

    fix sales

--- resources/views/administor/user/list.blade.php
+++ resources/views/administor/user/list.blade.php
@@ -97,7 +97,7 @@
 										<td>{{ $users->register_gadget }}</td>
 									</tr>
 								@endforeach
-								@foreach ($distributor->children as member)		
+								@foreach ($distributor->children as $member)		
 									@foreach ($member->admin_info_user_info as $users)
 										<tr>
 											<td>{{ $users->name }}</td>
