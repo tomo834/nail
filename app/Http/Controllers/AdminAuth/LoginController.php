17d7d3fc87c19c9dda7b345969d8664b67214c12
tomo <stomo834@gmail.com>
2020-10-26 05:25:50 +0900

    fix register page

--- app/Http/Controllers/AdminAuth/LoginController.php
+++ app/Http/Controllers/AdminAuth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
 use Hesto\MultiAuth\Traits\LogsoutGuard;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
