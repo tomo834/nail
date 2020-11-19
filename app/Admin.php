c3f1e8366c659b9391984ce529a157261a508f2a
tomo <stomo834@gmail.com>
2020-11-19 14:46:20 +0900

    fix import

--- app/Admin.php
+++ app/Admin.php
@@ -17,7 +17,7 @@ class Admin extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','type', 'name_jp',"company_name","representative","shop_name","shop_pic","address","phone","cellphone","account_holder","bank_name","branch_name","bank_code","branch_code","account_type","account_number","incentive","historical_matters","passbook","other","account_id","homepage","zip_code","shop_zip_code","shop_address","request","shop_phone","shop_open","has_nailist"
+        'name', 'email', 'password','type', 'name_jp',"company_name","representative","shop_name","shop_pic","address","phone","cellphone","account_holder","bank_name","branch_name","bank_code","branch_code","account_type","account_number","incentive","historical_matters","passbook","other","account_id","homepage","zip_code","shop_zip_code","shop_address","request","shop_phone","shop_open","has_nailist","notification_address"
     ];
     /**
      * The attributes that should be hidden for arrays.
